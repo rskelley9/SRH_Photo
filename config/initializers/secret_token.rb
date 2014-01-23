9aebfb5bed7d4ee6e441720ed0bfeecdf06ce9b6
Ryan Kelley <rskelley9@gmail.com>
2014-01-23 05:40:23 -0500

    fixed rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SRHPhoto1::Application.config.secret_key_base = 'f0433292fc26e17fba20d2e6ad9b5241b0554d6e30065c3ad5a8aee29d4aa382ef9964170600003af1d4e3ddc1a97cacd69bddbc85583339cee4f6068cb51d5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SRHPhoto1::Application.config.session_store :cookie_store, key: '_SRH_Photo1_session'
