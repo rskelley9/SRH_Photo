ff1e74aa0cb861c45326ccb21dd8d1f1128b4258
Ryan Kelley <rskelley9@gmail.com>
2014-01-27 07:03:13 -0500

    added logo to navbar, made some styling changes to navbar and homepage

--- app/controllers/rssfeed_controller.rb
+++ app/controllers/rssfeed_controller.rb
@@ -3,25 +3,11 @@
 
 class RssfeedController < ApplicationController
 
-  # def index
-  #   feed_url = 'http://www.npr.org/rss/rss.php?id=1001'
-  #   @output = "<h1>Recent Posts</h1>"
-  #   open(feed_url) do |http|
-  #     response = http.read
-  #     result = RSS::Parser.parse(response, false)
-  #     @output += "Feed Title: #{result.channel.title}<br />"
-  #     result.items.each_with_index do |item, i|
-  #       @output += "#{i+1}. <a href='#{item.link}'>#{item.title}</a><br />" if i < 10
-  #     end
-  #   end
-  #   render :_rss_feed
-  # end
-
   def index
     feed_url = 'http://www.npr.org/rss/rss.php?id=1001'
-    @rss_list = "<h1>Recent Posts</h1>"
+    header = "Recent Posts"
 
-    @rss_feed = Rssfeed.new(feed_url, @rss_list, 10)
+    @rss_feed = Rssfeed.new(feed_url, header, 10)
    render :_rss_feed
  end
 
