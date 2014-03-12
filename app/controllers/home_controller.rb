1922a720acedf5975635acd09f93869ec2e18e36
Ryan Kelley <rskelley9@gmail.com>
2014-03-12 05:18:11 -0400

    installing seriously annoying template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    render layout:"home_layout"
+    # render layout:"home_layout"
+      render "theme_test", layout:"theme_layout"
   end
 
 
